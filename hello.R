81ad21219a2d08bde78ba772ebd0095f462c8176
Christian Testa <ctesta@hsph.harvard.edu>
2022-12-28 15:40:04 -0500

    Improve hello.R

--- hello.R
+++ hello.R
@@ -1,3 +1,6 @@
-greetUser <- function() {
-  print("Hello World")
+#' Greet User is a function that will print "Hello World"
+#' 
+#' It takes an argument of the user's name
+greetUser <- function(username) {
+  print(paste0("Hello World, ", username))
 }
